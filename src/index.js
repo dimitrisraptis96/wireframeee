6e7e2b0bb488a58bfe0e2efca5c45ba509f52b3d
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-10-16 17:40:29 +0300

    Proven goddamn MVP here

--- src/index.js
+++ src/index.js
@@ -3,25 +3,57 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+function createArrayFromInt(range) {
+  var x = [];
+  for (var i = 1; i <= range; i++) {
+    x.push(i);
+  }
+  return x;
+}
+
+const MIN_WIDTH = 10;
+const MAX_WIDTH = 80;
+
 function App() {
+  const [refresh, setRefresh] = useState(false);
+  const refreshState = () => setRefresh(!refresh);
+
   const [radius, setRadius] = useState(2);
   const [lines, setLines] = useState(3);
-  const [words, setWords] = useState(0);
+  const [words, setWords] = useState(2);
   const [height, setHeight] = useState(12);
+  const [spacing, setSpacing] = useState(4);
 
-  console.log(Array(lines).fill());
+  function createRow(rowNum) {
+    var lastWidth = 0;
+    var totalWidth = 0;
+    const rects = createArrayFromInt(words).map((x, i) => {
+      const currentWidth = getRandom(MIN_WIDTH, MAX_WIDTH);
+      totalWidth += lastWidth;
+      lastWidth = currentWidth;
 
-  const rects = new Array(lines).fill().map((x, i) => {
+      return (
+        <rect
+          x={totalWidth + i * spacing}
+          y={height * rowNum + spacing * rowNum}
+          fill="#ddd"
+          width={currentWidth}
+          height={height}
+          rx={radius}
+        />
+      );
+    });
+
+    return rects;
+  }
+
+  const rects = createArrayFromInt(lines).map((row, index) => {
     return (
-      <rect
-        // x={200 * i + 8 * i}
-        y={height * i + 8 * i}
-        fill="red"
-        width={getRandom(30, 100)}
-        height={height}
-        rx={radius}
-        ry={radius}
-      />
+      <g key={index}>
+        {createRow(index).map(rect => (
+          <>{rect}</>
+        ))}
+      </g>
     );
   });
 
@@ -38,6 +70,7 @@ function App() {
           onChange={e => setRadius(e.target.value)}
           value={radius}
         />
+        {radius}
       </div>
       <div>
         <label>Max Words per line: </label>
@@ -50,6 +83,7 @@ function App() {
           onChange={e => setWords(e.target.value)}
           value={words}
         />
+        {words}
       </div>
       <div>
         <label>Lines</label>
@@ -62,6 +96,7 @@ function App() {
           onChange={e => setLines(e.target.value)}
           value={lines}
         />
+        {lines}
       </div>
       <div>
         <label>Height: </label>
@@ -74,13 +109,31 @@ function App() {
           onChange={e => setHeight(e.target.value)}
           value={height}
         />
+        {height}
       </div>
+      <div>
+        <label>Spacing: </label>
+        <input
+          id="spacing"
+          type="range"
+          min={4}
+          max={20}
+          step={1}
+          onChange={e => setSpacing(e.target.value)}
+          value={spacing}
+        />
+        {spacing}
+      </div>
+      <button onClick={refreshState}>Refresh</button>
 
       <div style={{ margin: "4rem" }} />
 
-      <svg width="200" height={height * lines + 8 * lines}>
+      <svg
+        width={MAX_WIDTH * (words + 1) + spacing * (words + 1)}
+        height={height * lines + spacing * lines}
+      >
         {rects.map((rect, index) => (
-          <g key={index}>{rect}</g>
+          <>{rect}</>
         ))}
       </svg>
     </div>
