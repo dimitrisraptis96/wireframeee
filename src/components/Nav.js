17d88b80729564742f91ad44e81789fc18a97cb2
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-10-21 18:43:00 +0300

    :tada: add seperate nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Flex, Box } from "rebass/styled-components";
 
+import Link from "./Link";
 import Toggle from "./Toggle";
 import sun from "../assets/sun.png";
 import moon from "../assets/moon.png";
@@ -8,43 +9,62 @@ import moon from "../assets/moon.png";
 function Nav({ themeMode, setThemeMode }) {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <Flex justifyContent="space-between" p={4}>
+        <div>
+          <h1 style={{ color: "#3D50FC" }}>Wireframer</h1>
+        </div>
 
-      <div>
-        <Toggle
-          defaultChecked={themeMode === "dark" ? true : false}
-          onChange={() => setThemeMode(!themeMode)}
-          icons={{
-            checked: (
-              <img
-                style={{ pointerEvents: "none" }}
-                width="16"
-                height="16"
-                alt="moon"
-                aria-hidden
-                src={moon}
-              />
-            ),
-            unchecked: (
-              <img
-                style={{ pointerEvents: "none" }}
-                width="16"
-                height="16"
-                alt="sun"
-                aria-hidden
-                src={sun}
-              />
-            )
-          }}
-        />
-      </div>
+        <Flex>
+          <ul style={{ listStyleType: "none", marginRight: "4rem" }}>
+            <Flex>
+              <li style={{ marginRight: "4rem" }}>
+                <Link to="/">
+                  <b>Home</b>
+                </Link>
+              </li>
+              <li style={{ marginRight: "4rem" }}>
+                <Link to="/about">
+                  <b>About</b>
+                </Link>
+              </li>
+              <li>
+                <Link to="/about">
+                  <b>Settings</b>
+                </Link>
+              </li>
+            </Flex>
+          </ul>
+
+          <div>
+            <Toggle
+              defaultChecked={themeMode === "dark" ? true : false}
+              onChange={() => setThemeMode(!themeMode)}
+              icons={{
+                checked: (
+                  <img
+                    style={{ pointerEvents: "none" }}
+                    width="16"
+                    height="16"
+                    alt="moon"
+                    aria-hidden
+                    src={moon}
+                  />
+                ),
+                unchecked: (
+                  <img
+                    style={{ pointerEvents: "none" }}
+                    width="16"
+                    height="16"
+                    alt="sun"
+                    aria-hidden
+                    src={sun}
+                  />
+                )
+              }}
+            />
+          </div>
+        </Flex>
+      </Flex>
     </nav>
   );
 }
