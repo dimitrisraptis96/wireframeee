eb60de899ec1c0a58309fb9e6866f1802ece19c4
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-12-02 20:33:48 +0200

    :rocket: Add Product of the Day badge

--- src/components/PHBanner.js
+++ src/components/PHBanner.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 const POST_ID = "176300";
+const IS_DARK = true;
 
 export const PHBanner = () => {
   return (
     <a
-      href="https://www.producthunt.com/posts/wireframer"
+      href="https://www.producthunt.com/posts/wireframer?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-wireframer"
       target="_blank"
       rel="noreferrer"
     >
       <img
-        src={`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=${POST_ID}&theme=dark`}
+        src={`https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=${POST_ID}&theme=${
+          IS_DARK ? "dark" : "light"
+        }&period=daily`}
         alt="Wireframer | Flawless SVG text for your wireframes | Product Hunt Embed"
         style={{
           zIndex: "100000000",
